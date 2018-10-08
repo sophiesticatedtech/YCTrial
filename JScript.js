8ed8c1b1b7d4a594202f03b8e9b5c053b6ed4034
Sophie de Haan <sophiedehaan1@gmail.com>
2018-10-08 15:33:06 +0200

    first test

---
+++ JScript.js
@@ -0,0 +1,37 @@
+const JScriptVoorbeeld = ()=>{
+    alert("Disco time!");
+    document.body.style.backgroundColor="yellow";
+
+    // for (i = 0 ; i < 8; i++) {
+    //     if(i <= 3){
+    //         document.body.style.backgroundColor="blue";
+    //         console.log("We are blue");
+    //     } else {
+    //         document.body.style.backgroundColor="lightgreen";
+    //         console.log("We are lightgreen");
+    //     };
+    //     setTimeout(3000);
+    // };
+
+};
+
+function myColor1(){
+        document.body.style.backgroundColor="red";
+        console.log("We are red");
+    }
+
+function myColor2(){
+        document.body.style.backgroundColor="green";
+        console.log("We are green");
+    }
+
+function myColor3(){
+        document.body.style.backgroundColor="blue";
+        console.log("We are blue");
+    }
+// a = 5;
+// switch (a) {
+//     case 1: document.body.style.backgroundColor="red"; break;
+//     case 2: document.body.style.backgroundColor="red"; break;
+//     case 3: document.body.style.backgroundColor="red"; break;
+// }
